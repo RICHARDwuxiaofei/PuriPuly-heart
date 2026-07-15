e0ddd40301f4acab538df8523459041a7f5cf15e
salee <kapitalismho@gmail.com>
2026-07-15 18:41:38 +0900

    docs: add Welcius to Special Thanks in README and app About page

--- src/puripuly_heart/ui/views/about.py
+++ src/puripuly_heart/ui/views/about.py
@@ -238,6 +238,7 @@ class AboutView(ft.Column):
         "about.special_thanks.name.ephedrine",
         "about.special_thanks.name.eri",
         "about.special_thanks.name.fzcfweasdferttgg_png",
+        "about.special_thanks.name.welcius",
     ]
 
     def _build_special_thanks_card(self) -> ft.Control:
